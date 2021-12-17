de2fd763e3e5cb4d3eaabdddc13eba3c1ba45001
Asher Chan <asher_chan@foxmail.com>
2021-12-17 11:35:34 +0800

    Update video2anime.py

--- video2anime.py
+++ video2anime.py
@@ -1,7 +1,3 @@
-'''
-   made by @finnkso (github)
-   2020.04.09
-'''
 import argparse
 import os
 import cv2
@@ -9,7 +5,6 @@ from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
 from net import generator
-from tools.utils import preprocessing, check_folder
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -31,19 +26,27 @@ def parse_args():
     return parser.parse_args()
 
 
-def convert_image(img, img_size):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = preprocessing(img, img_size)
-    img = np.expand_dims(img, axis=0)
-    img = np.asarray(img)
+def check_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+def process_image(img, x32=True):
+    h, w = img.shape[:2]
+    if x32: # resize image to multiple of 32s
+        def to_32s(x):
+            return 256 if x < 256 else x - x%32
+        img = cv2.resize(img, (to_32s(w), to_32s(h)))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)/ 127.5 - 1.0
     return img
 
-def inverse_image(img):
+def post_precess(img, wh):
     img = (img.squeeze()+1.) / 2 * 255
     img = img.astype(np.uint8)
+    img = cv2.resize(img, (wh[0], wh[1]))
     return img
 
-def cvt2anime_video(video, output, checkpoint_dir, output_format='MP4V', img_size=(256,256)):
+def cvt2anime_video(video, output, checkpoint_dir, output_format='MP4V'):
     '''
     output_format: 4-letter code that specify codec to use for specific video type. e.g. for mp4 support use "H264", "MP4V", or "X264"
     '''
@@ -53,16 +56,18 @@ def cvt2anime_video(video, output, checkpoint_dir, output_format='MP4V', img_siz
     gpu_options = tf.GPUOptions(allow_growth=gpu_stat)
 
     test_real = tf.placeholder(tf.float32, [1, None, None, 3], name='test')
-
     with tf.variable_scope("generator", reuse=False):
         test_generated = generator.G_net(test_real).fake
+         
+    saver = tf.train.Saver()
 
     # load video
     vid = cv2.VideoCapture(video)
     vid_name = os.path.basename(video)
     total = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(vid.get(cv2.CAP_PROP_FPS))
-    # codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
     codec = cv2.VideoWriter_fourcc(*output_format)
 
     tfconfig = tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)
@@ -70,7 +75,6 @@ def cvt2anime_video(video, output, checkpoint_dir, output_format='MP4V', img_siz
         # tf.global_variables_initializer().run()
         # load model
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)  # checkpoint file information
-        saver = tf.train.Saver()
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)  # first line
             saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
@@ -78,37 +82,28 @@ def cvt2anime_video(video, output, checkpoint_dir, output_format='MP4V', img_siz
         else:
             print(" [*] Failed to find a checkpoint")
             return
+         
+        video_out = cv2.VideoWriter(os.path.join(output, vid_name.rsplit('.', 1)[0] + "_AnimeGANv2.mp4"), codec, fps, (width, height))
 
-        # determine output width and height
-        ret, img = vid.read()
-        if img is None:
-            print('Error! Failed to determine frame size: frame empty.')
-            return
-        img = preprocessing(img, img_size)
-        height, width = img.shape[:2]
-        # out = cv2.VideoWriter(os.path.join(output, vid_name.replace('mp4','mkv')), codec, fps, (width, height))
-        out = cv2.VideoWriter(os.path.join(output, vid_name), codec, fps, (width, height))
-
-        pbar = tqdm(total=total)
-        vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        while ret:
+        pbar = tqdm(total=total, ncols=80)
+        pbar.set_description(f"Making: {os.path.basename(video).rsplit('.', 1)[0] + '_AnimeGANv2.mp4'}")
+        while True:
             ret, frame = vid.read()
-            if frame is None:
-                print('Warning: got empty frame.')
-                continue
-            img = convert_image(frame, img_size)
-            fake_img = sess.run(test_generated, feed_dict={test_real: img})
-            fake_img = inverse_image(fake_img)
-            fake_img = cv2.resize(fake_img, (width, height))
-            out.write(cv2.cvtColor(fake_img, cv2.COLOR_RGB2BGR))
+            if not ret:
+                break
+            frame = np.asarray(np.expand_dims(process_image(frame),0))
+            fake_img = sess.run(test_generated, feed_dict={test_real: frame})
+            fake_img = post_precess(fake_img, (width, height))
+            video_out.write(cv2.cvtColor(fake_img, cv2.COLOR_BGR2RGB))
             pbar.update(1)
 
         pbar.close()
         vid.release()
-        return os.path.join(output, vid_name)
+        video_out.release()
+        return os.path.join(output, vid_name.rsplit('.', 1)[0] + "_AnimeGANv2.mp4")
 
 if __name__ == '__main__':
     arg = parse_args()
     check_folder(arg.output)
-    info = cvt2anime_video(arg.video, arg.output, arg.checkpoint_dir, output_format=arg.output_format)
+    info = cvt2anime_video(arg.video, arg.output, arg.checkpoint_dir)
     print(f'output video: {info}')
